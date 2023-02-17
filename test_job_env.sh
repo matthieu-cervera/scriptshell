09d861feac7a953a55a1f3d41b766a0e6afca392
matthieu-cervera <73890429+matthieu-cervera@users.noreply.github.com>
2023-02-17 17:39:58 +0100

    Add files via upload

--- test_job_env.sh
+++ test_job_env.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --time=00:10:00
+#SBATCH --time=00:01:00
 #SBATCH --account=def-pesantg	
 #SBATCH --output=testenv-%J.out
 #SBATCH --mail-user=matthieu.cervera@hotmail.com
 #SBATCH --mail-type=ALL
 
 source pyth-env/bin/activate
-python3 pip freeze > installed.txt
+python3 -m pip freeze > installed.txt
 echo 'Python environment still here'
 deactivate
 
