07e76a9b34c4ce5171cd5ab61f716dda4241d58f
matthieu-cervera <73890429+matthieu-cervera@users.noreply.github.com>
2023-02-20 18:51:01 +0100

    Add files via upload

--- test_job.sh
+++ test_job.sh
@@ -6,11 +6,18 @@
 #SBATCH --mail-user=matthieu.cervera@hotmail.com
 #SBATCH --mail-type=ALL
 
-python3 -m venv pyth-env
-source pyth-env/bin/activate
-python3 -m pip install -r requirements.txt
-echo 'Python environment in place'
-deactivate
+cp requirements.txt $SLURM_TMPDIR/
 
+module load python/3.8
+module load scipy-stack
+virtualenv --no-download $SLURM_TMPDIR/env
+source $SLURM_TMPDIR/env/bin/activate
+pip install --upgrade pip --no-index
+pip install -r requirements.txt
 
+cd $SLURM_TMPDIR/
+unzip CMT_test.zip -d $SLURM_TMPDIR/
+unzip /home/matt3c/scratch/pkl_files_EWLD.zip -d $SLURM_TMPDIR/
+
+ls 
 exit
