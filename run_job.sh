f422c084021770b0268e2c3c47ce7b539f901f2e
matthieu-cervera <73890429+matthieu-cervera@users.noreply.github.com>
2023-02-20 18:28:20 +0100

    Add files via upload

---
+++ run_job.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH --account=def-pesantg
+#SBATCH --mail-user=matthieu.cervera@hotmail.com
+#SBATCH --mail-type=ALL
+#SBATCH --gres=gpu:v100l:1 # Request GPU "generic resources"
+#SBATCH --cpus-per-task=8  # Cores proportional to GPUs: 6 on Cedar, 16 on Graham.
+#SBATCH --mem=48000M       # Memory proportional to GPUs: 32000 Cedar, 64000 Graham.
+#SBATCH --time=0-02:00
+
+cp requirements.txt $SLURM_TMPDIR/
+
+module load python/3.8
+module load scipy-stack
+virtualenv --no-download $SLURM_TMPDIR/env
+source $SLURM_TMPDIR/env/bin/activate
+pip install --upgrade pip --no-index
+pip install -r requirements.txt
+
+cd $SLURM_TMPDIR/
+unzip CMT_test.zip -d $SLURM_TMPDIR/
+unzip /home/matt3c/scratch/pkl_files_EWLD.zip -d $SLURM_TMPDIR/
+
+python $SLURM_TMPDIR/run.py --idx 2 --gpu_index 0 --ngpu 1 --optim_name adam --restore_epoch -1 --seed 42
+
+zip -r /home/matt3c/scratch/training_P1_testing results/idx002/
+
+exit
