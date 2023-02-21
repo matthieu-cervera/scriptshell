73810e9053af38f634498a736b5f0a232bb52024
matthieu-cervera <73890429+matthieu-cervera@users.noreply.github.com>
2023-02-21 22:20:31 +0100

    Add files via upload

--- usage.py
+++ usage.py
@@ -1,5 +1,5 @@
 import psutil
 
-logger.info("")
-logger.info("Memory%" % psutil.virtual_memory()[3])
-logger.info("CPU%" % psutil.cpu_percent(interval=1))
+print("")
+print("Memory%" + str(psutil.virtual_memory()[3]))
+print("CPU%" + str(psutil.cpu_percent(interval=1)))
