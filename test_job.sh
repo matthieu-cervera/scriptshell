c8fe95b0cb450815c78c8f1390c9d06348abea7e
matthieu-cervera <73890429+matthieu-cervera@users.noreply.github.com>
2023-02-23 16:42:58 +0100

    Add files via upload

--- test_job.sh
+++ test_job.sh
@@ -12,8 +12,8 @@ module load python/3.8
 module load scipy-stack
 virtualenv --no-download $SLURM_TMPDIR/env
 source $SLURM_TMPDIR/env/bin/activate
-#pip install --upgrade pip --no-index
-#pip install -r requirements.txt
+pip install --upgrade pip --no-index
+pip install -r requirements.txt
 pip freeze > installed.txt
 cd $SLURM_TMPDIR/
 
