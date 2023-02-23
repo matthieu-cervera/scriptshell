8cb00516db3c0d4dfea7f9747d02824392c98891
matthieu-cervera <73890429+matthieu-cervera@users.noreply.github.com>
2023-02-23 16:27:31 +0100

    Add files via upload

--- test_job.sh
+++ test_job.sh
@@ -12,14 +12,12 @@ module load python/3.8
 module load scipy-stack
 virtualenv --no-download $SLURM_TMPDIR/env
 source $SLURM_TMPDIR/env/bin/activate
-pip install --upgrade pip --no-index
-pip install -r requirements.txt
+#pip install --upgrade pip --no-index
+#pip install -r requirements.txt
 pip freeze > installed.txt
-
-cat installed.txt
-
 cd $SLURM_TMPDIR/
 
+cat installed.txt
+cp installed.txt projects/def-pesantg/matt3c/scriptshell
 
-ls 
 exit
