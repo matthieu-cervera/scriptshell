103dc32c949cae3de4355cc06c4575c5e6b52376
matthieu-cervera <73890429+matthieu-cervera@users.noreply.github.com>
2023-02-28 20:58:29 +0100

    Add files via upload

--- run_job_gen.sh
+++ run_job_gen.sh
@@ -24,6 +24,7 @@ unset JAVA_TOOL_OPTIONS
 cd $SLURM_TMPDIR/
 #unzip /home/manibod/scratch/CMTCP_R7_P3_6token3_refactor.zip -d $SLURM_TMPDIR/
 #unzip /home/manibod/projects/def-pesantg/manibod/static_data.zip -d $SLURM_TMPDIR/
+unzip /home/matt3c/projects/def-pesantg/matt3c/scriptshell/CMT-usagelog-csv.zip -d $SLURM_TMPDIR/
 unzip /home/matt3c/scratch/pkl_files_EWLD.zip -d $SLURM_TMPDIR/
 
 python $SLURM_TMPDIR/run.py --idx 2 --gpu_index 0 --ngpu 1 --optim_name adam --restore_epoch -100 --seed 42 --load_rhythm --sample
