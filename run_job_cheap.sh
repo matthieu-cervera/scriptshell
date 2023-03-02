831bff8dbcd481f615bacb12ab9cc6dc348f0a1f
matthieu-cervera <73890429+matthieu-cervera@users.noreply.github.com>
2023-03-02 17:46:58 +0100

    Add files via upload

--- run_job_cheap.sh
+++ run_job_cheap.sh
@@ -2,6 +2,7 @@
 #SBATCH --account=def-pesantg
 #SBATCH --mail-user=matthieu.cervera@hotmail.com
 #SBATCH --mail-type=ALL
+#SBATCH --gres=gpu:v100l:1 # Request GPU "generic resources"
 #SBATCH --cpus-per-task=4  # Cores proportional to GPUs: 6 on Cedar, 16 on Graham.
 #SBATCH --mem=24000M       # Memory proportional to GPUs: 32000 Cedar, 64000 Graham.
 #SBATCH --time=1-10:00
