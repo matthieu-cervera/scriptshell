194d0e8b41c79323a2a72a136425eeafde2d2a65
matthieu-cervera <73890429+matthieu-cervera@users.noreply.github.com>
2023-03-03 17:38:55 +0100

    Add files via upload

--- run_job_cheap.sh
+++ run_job_cheap.sh
@@ -3,8 +3,8 @@
 #SBATCH --mail-user=matthieu.cervera@hotmail.com
 #SBATCH --mail-type=ALL
 #SBATCH --gres=gpu:v100l:1 # Request GPU "generic resources"
-#SBATCH --cpus-per-task=4  # Cores proportional to GPUs: 6 on Cedar, 16 on Graham.
-#SBATCH --mem=24000M       # Memory proportional to GPUs: 32000 Cedar, 64000 Graham.
+#SBATCH --cpus-per-task=2  # Cores proportional to GPUs: 6 on Cedar, 16 on Graham.
+#SBATCH --mem=12000M       # Memory proportional to GPUs: 32000 Cedar, 64000 Graham.
 #SBATCH --time=1-10:00
 
 cp requirements.txt usage.csv $SLURM_TMPDIR/
