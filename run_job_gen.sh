3ad71f081b757638bd4d3b8fe5a28465b84a2021
matthieu-cervera <73890429+matthieu-cervera@users.noreply.github.com>
2023-03-07 22:20:54 +0100

    Add files via upload

--- run_job_gen.sh
+++ run_job_gen.sh
@@ -3,11 +3,10 @@
 #SBATCH --mail-user=matthieu.cervera@hotmail.com
 #SBATCH --mail-type=ALL
 #SBATCH --gres=gpu:v100l:1 # Request GPU "generic resources"
-#SBATCH --cpus-per-task=8  # Cores proportional to GPUs: 6 on Cedar, 16 on Graham.
-#SBATCH --mem=48000M       # Memory proportional to GPUs: 32000 Cedar, 64000 Graham.
-#SBATCH --time=0-10:00
+#SBATCH --cpus-per-task=1  # Cores proportional to GPUs: 6 on Cedar, 16 on Graham.
+#SBATCH --time=0-00:45
 
-cp requirements.txt $SLURM_TMPDIR/
+cp requirements.txt usage-thread.csv $SLURM_TMPDIR/
 
 module load python/3.8
 module load scipy-stack
@@ -16,26 +15,30 @@ module load maven
 virtualenv --no-download $SLURM_TMPDIR/env
 source $SLURM_TMPDIR/env/bin/activate
 pip install --upgrade pip --no-index
-pip install -r requirements.txt
+pip install -q -r requirements.txt
 
 java -Xms256m -Xmx4g -version
 unset JAVA_TOOL_OPTIONS
 
 cd $SLURM_TMPDIR/
-#unzip /home/manibod/scratch/CMTCP_R7_P3_6token3_refactor.zip -d $SLURM_TMPDIR/
-#unzip /home/manibod/projects/def-pesantg/manibod/static_data.zip -d $SLURM_TMPDIR/
-unzip /home/matt3c/projects/def-pesantg/matt3c/scriptshell/CMT-usagelog-csv.zip -d $SLURM_TMPDIR/
-#unzip /home/matt3c/scratch/pkl_files_EWLD.zip -d $SLURM_TMPDIR/
-unzip /home/matt3c/projects/def-pesantg/matt3c/scriptshell/results.zip -d $SLURM_TMPDIR/
+
+unzip -q /home/matt3c/projects/def-pesantg/matt3c/scriptshell/CMT-thread.zip -d $SLURM_TMPDIR/
+unzip -q /home/matt3c/projects/def-pesantg/matt3c/scriptshell/results.zip -d $SLURM_TMPDIR/
+unzip -q /home/matt3c/scratch/pkl_files_EWLD.zip -d $SLURM_TMPDIR/
+
+rm -r results/idx002/sampling_results/
+
+cd $SLURM_TMPDIR/
+
+python $SLURM_TMPDIR/run_w_threads.py --idx 2 --gpu_index 0 --ngpu 1 --optim_name adam --restore_epoch -100 --seed 42 --load_rhythm --sample
 
 
-python $SLURM_TMPDIR/run.py --idx 2 --gpu_index 0 --ngpu 1 --optim_name adam --restore_epoch -100 --seed 42 --load_rhythm --sample
+cd $SLURM_TMPDIR/
 
-zip -r results.zip results/idx002/
+zip -q -r results_sampling.zip results/idx002/sampling_results/
 
-#zip -r /home/manibod/scratch/CMTCP_R7_P3_6token3_refactor_results results/idx002/sampling_results/
 
-cp results.zip /home/matt3c/projects/def-pesantg/matt3c/scriptshell/
-#cp usage_gen.csv /home/matt3c/projects/def-pesantg/matt3c/scriptshell/
+cp results_sampling.zip /home/matt3c/projects/def-pesantg/matt3c/scriptshell/
+cp usage-thread.csv /home/matt3c/projects/def-pesantg/matt3c/scriptshell/
 
 exit
