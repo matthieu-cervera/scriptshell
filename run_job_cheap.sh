79f3745627e2eef0f1c66c7eb683b9f8ef5880a6
matthieu-cervera <73890429+matthieu-cervera@users.noreply.github.com>
2023-03-07 18:43:17 +0100

    Add files via upload

--- run_job_cheap.sh
+++ run_job_cheap.sh
@@ -4,27 +4,27 @@
 #SBATCH --mail-type=ALL
 #SBATCH --gres=gpu:v100l:1 # Request GPU "generic resources"
 #SBATCH --mem=12000M       # Memory proportional to GPUs: 32000 Cedar, 64000 Graham.
-#SBATCH --time=1-10:00
+#SBATCH --time=0-10:00
 
-cp requirements.txt usage.csv $SLURM_TMPDIR/
+cp requirements.txt usage-train-thread.csv $SLURM_TMPDIR/
 
 module load python/3.8
 module load scipy-stack
 virtualenv --no-download $SLURM_TMPDIR/env
 source $SLURM_TMPDIR/env/bin/activate
 pip install --upgrade pip --no-index
-pip install -r requirements.txt
+pip install -q -r requirements.txt
 
 cd $SLURM_TMPDIR/
-unzip /home/matt3c/projects/def-pesantg/matt3c/scriptshell/CMT-usagelog-csv.zip -d $SLURM_TMPDIR/
-unzip /home/matt3c/scratch/pkl_files_EWLD.zip -d $SLURM_TMPDIR/
+unzip -q /home/matt3c/projects/def-pesantg/matt3c/scriptshell/CMT-train-thread.zip -d $SLURM_TMPDIR/
+unzip -q /home/matt3c/scratch/pkl_files_EWLD.zip -d $SLURM_TMPDIR/
 
-python $SLURM_TMPDIR/run.py --idx 2 --gpu_index 0 --ngpu 1 --optim_name adam --restore_epoch -1 --seed 42
+python $SLURM_TMPDIR/run_w_threads.py --idx 2 --gpu_index 0 --ngpu 1 --optim_name adam --restore_epoch -1 --seed 42
 
 zip -r resultscheap.zip results/idx002/
 
 
-cp resultscheap.zip /home/matt3c/projects/def-pesantg/matt3c/scriptshell/
-cp usage.csv /home/matt3c/projects/def-pesantg/matt3c/scriptshell/
+cp -q resultscheap.zip /home/matt3c/projects/def-pesantg/matt3c/scriptshell/
+cp -q usage-train-thread.csv /home/matt3c/projects/def-pesantg/matt3c/scriptshell/
 
 exit
