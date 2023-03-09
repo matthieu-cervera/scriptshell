77661845ea5d4181420dccf3c773e5323ab3b70e
matthieu-cervera <73890429+matthieu-cervera@users.noreply.github.com>
2023-03-09 19:36:50 +0100

    Add files via upload

---
+++ run_job_constrained.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#SBATCH --account=def-pesantg
+#SBATCH --mail-user=matthieu.cervera@hotmail.com
+#SBATCH --mail-type=ALL
+#SBATCH --gres=gpu:v100l:1 # Request GPU "generic resources"
+#SBATCH --mem=12000M       # Memory proportional to GPUs: 32000 Cedar, 64000 Graham.
+#SBATCH --time=0-10:00
+
+cp requirements.txt usage-train-thread.csv $SLURM_TMPDIR/
+
+module load python/3.8
+module load scipy-stack
+virtualenv --no-download $SLURM_TMPDIR/env
+source $SLURM_TMPDIR/env/bin/activate
+pip install --upgrade pip --no-index
+pip install -q -r requirements.txt
+
+cd $SLURM_TMPDIR/
+unzip -q /home/matt3c/projects/def-pesantg/matt3c/scriptshell/CMT-train-constrained.zip -d $SLURM_TMPDIR/
+unzip -q /home/matt3c/scratch/pkl_files_EWLD.zip -d $SLURM_TMPDIR/
+
+python $SLURM_TMPDIR/run_w_threads.py --idx 2 --gpu_index 0 --ngpu 1 --optim_name adam --restore_epoch -1 --seed 42
+
+zip -q -r resultsconstrained.zip results/idx002/
+
+
+cp resultsconstrained.zip /home/matt3c/projects/def-pesantg/matt3c/scriptshell/
+cp usage-train-constrained.csv /home/matt3c/projects/def-pesantg/matt3c/scriptshell/
+
+exit
