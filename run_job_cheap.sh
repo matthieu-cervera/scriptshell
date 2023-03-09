36bf714a8a7f3adccc897a86a25510401b5c2509
matthieu-cervera <73890429+matthieu-cervera@users.noreply.github.com>
2023-03-09 16:46:46 +0100

    Add files via upload

--- run_job_cheap.sh
+++ run_job_cheap.sh
@@ -21,10 +21,10 @@ unzip -q /home/matt3c/scratch/pkl_files_EWLD.zip -d $SLURM_TMPDIR/
 
 python $SLURM_TMPDIR/run_w_threads.py --idx 2 --gpu_index 0 --ngpu 1 --optim_name adam --restore_epoch -1 --seed 42
 
-zip -r resultscheap.zip results/idx002/
+zip -q -r resultscheap.zip results/idx002/
 
 
-cp -q resultscheap.zip /home/matt3c/projects/def-pesantg/matt3c/scriptshell/
-cp -q usage-train-thread.csv /home/matt3c/projects/def-pesantg/matt3c/scriptshell/
+cp resultscheap.zip /home/matt3c/projects/def-pesantg/matt3c/scriptshell/
+cp usage-train-thread.csv /home/matt3c/projects/def-pesantg/matt3c/scriptshell/
 
 exit
